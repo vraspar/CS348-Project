b6e977a6b3e11a9b2cb8179d4d9a51367b65d009
eandr127 <ryan.larkin@uwaterloo.ca>
2023-03-05 14:53:47 -0500

    Backend starter code

---
+++ Backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
@@ -0,0 +1,11 @@
+package cs348.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cs348BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<Cs348BackendApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/cs348/backend/Cs348BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package cs348.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Cs348BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
