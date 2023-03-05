a5e50d3dd06bf0b887aee35e75521148cdde7ac5
eandr127 <ryan.larkin@uwaterloo.ca>
2023-03-05 16:19:53 -0500

    Add controller connecting to DB

--- Backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
+++ Backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
@@ -1,11 +1,26 @@
 package cs348.backend
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Cs348BackendApplication
 
 fun main(args: Array<String>) {
 	runApplication<Cs348BackendApplication>(*args)
+}
+
+@RestController
+class TestController(val jdbcTemplate: JdbcTemplate) {
+	@GetMapping("/")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
+
+	@GetMapping("/test")
+	fun test() = jdbcTemplate.query("SELECT * FROM STUDENT") { rs, _ -> rs.getString("NAME") }
 }
