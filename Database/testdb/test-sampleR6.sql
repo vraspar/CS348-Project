66dc49d0444c5357ea97d2733c1a75e16d578916
Davi Sena <davisena145@gmail.com>
2023-03-06 18:40:17 -0500

    adding R6 sql and out queries

---
+++ Database/testdb/test-sampleR6.sql
@@ -0,0 +1,4 @@
+select player.player_name as player_name, PLUS_MINUS, team.ABBREVIATION as team_name 
+from player_in_game_stat, player, team 
+WHERE player.ID = player_in_game_stat.player_ID and team.id = player_in_game_stat.team_ID AND 
+PLUS_MINUS = (SELECT MAX(PLUS_MINUS) FROM player_in_game_stat as p2 WHERE team.id = p2.team_id)
