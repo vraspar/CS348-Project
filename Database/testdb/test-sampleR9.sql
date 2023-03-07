df54487b0e3392759e5a3146ae46297f08ac7f33
vraspar <v2parikh@uwaterloo.ca>
2023-03-06 23:02:50 -0500

    add R9 sample sql query

---
+++ Database/testdb/test-sampleR9.sql
@@ -0,0 +1,8 @@
+-- Get # winning games for Celtics(1610612738)
+-- when playing home against Hawks(1610612737)
+	
+SELECT COUNT(*) 
+FROM Game 
+WHERE HOME_TEAM_ID = 1610612738 AND 
+    VISITOR_TEAM_ID = 1610612737 AND 
+    PTS_home > PTS_away
