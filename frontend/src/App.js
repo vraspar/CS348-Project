04654cb155e89a63cca1623d7af29303f538c521
vraspar <v2parikh@uwaterloo.ca>
2023-03-23 01:16:50 -0400

    fix typ0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import QueryPage from './components/queryPage';
-import env from 'react-dotenv';
 function App() {
   
   return (
     <div>
-    {env.SERVER_URL}
+    
     <QueryPage />
     </div>
   );
