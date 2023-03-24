ceec7ebe2b7d53863a0d48383481c1da9dbf1da1
Davi Sena <davisena145@gmail.com>
2023-03-24 18:17:20 -0400

    implementing R6

--- backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
+++ backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.jdbc.datasource.init.ScriptUtils
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 import java.sql.ResultSet
@@ -38,6 +39,13 @@ data class AveragePlayerSize(
     val state: String,
     val height: Double,
     val weight: Double,
+)
+
+data class Player_in_game_stat(
+    val name: String?,
+    val stat: String?,
+    val team_name: String?,
+    val date: String?
 )
 
 data class QueryResult(
@@ -103,6 +111,28 @@ class NbaStatsService(val db: JdbcTemplate) {
             state
         ).firstOrNull()
 
+    
+    fun bestTeamStatInAllMatches(stat: String): List<Player_in_game_stat> {
+        val queryString = """
+        select player.player_name as player_name, $stat, team.ABBREVIATION as team_name, GAME_DATE_EST as date
+        from player_in_game_stat, player, team, Game 
+        WHERE player.ID = player_in_game_stat.player_ID and team.id = player_in_game_stat.team_ID and 
+        Game.ID = player_in_game_stat.GAME_ID and
+        $stat = (SELECT MAX($stat) FROM player_in_game_stat as p2 WHERE team.id = p2.team_id)
+        """;
+        return db.query(
+            queryString,
+            { response, _ ->
+                Player_in_game_stat(
+                    response.getString("PLAYER_NAME"),
+                    response.getString(stat),
+                    response.getString("team_name"),
+                    response.getString("date")
+                )
+            }
+        );
+    }
+
     fun runRawQuery(query: String) =
         if (!query.trimStart().startsWith("SELECT", ignoreCase = true)) {
             throw IllegalSqlModificationException("")
@@ -139,6 +169,9 @@ class StatsController(val service: NbaStatsService) {
 
     @GetMapping("/averages")
     fun averages() = service.findPlayerAverageSizesByState()
+
+    @GetMapping("/bestTeamStatInAllMatches")
+    fun bestTeamStatInAllMatches(@RequestParam("stat") stat: String) = service.bestTeamStatInAllMatches(stat)
 
     @ExceptionHandler
     fun handleIllegalSqlModificationException(ex: IllegalSqlModificationException) =
