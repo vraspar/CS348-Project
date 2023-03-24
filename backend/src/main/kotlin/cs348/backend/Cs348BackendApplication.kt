e49b4e4fb5e0c547003e00ae257ee0ba723541b6
Davi Sena <davisena145@gmail.com>
2023-03-24 18:19:07 -0400

    refactoring

--- backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
+++ backend/src/main/kotlin/cs348/backend/Cs348BackendApplication.kt
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.jdbc.datasource.init.ScriptUtils
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 import java.sql.ResultSet
